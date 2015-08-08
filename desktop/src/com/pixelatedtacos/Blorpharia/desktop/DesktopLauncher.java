3583e8d650c88720d6dab11638b94a4cd8ff0acc
Nucleria <lucysutter19@gmail.com>
2015-08-08 14:22:00 -0400

    Basic LibGDX/LWJGL2 setup.
    I set up the core/desktop versions of LibGDX with Gradle and here it
    is. Pretty sure that this a good .gitignore too. We’ll see.

---
+++ desktop/src/com/pixelatedtacos/Blorpharia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pixelatedtacos.Blorpharia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pixelatedtacos.Blorpharia.blorpharia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new blorpharia(), config);
+	}
+}
